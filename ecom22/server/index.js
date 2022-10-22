05d98112ffd4fd914294137ada8d6b1fe9a2699f
Sergio Rosa <scarlosjazz@gmail.com>
2022-10-22 19:40:37 -0400

    create express server

---
+++ ecom22/server/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/users", function(req, res) {
+  res.json({ 
+    data: "Sergio Rosa"
+
+  });
+
+});
+
+app.listen(7777, function() {
+  console.log("Server is running on port 7777");
+});
